abd0deba96f2cf85027d8f6ce793bb035193e633
Harshit <hktiwari980@gmail.com>
2025-09-23 00:23:09 +0530

    basic UI ready

---
+++ frontend/mern-todo/src/App.jsx
@@ -0,0 +1,57 @@
+import { useState } from 'react'
+import './App.css'
+import axios from 'axios';
+function App() {
+  const [newTodo, setNewTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+   const addTodo = async (e) =>{
+     e.preventDefault();
+     if(!newTodo.trim()) return;
+
+   try{
+     const response = await axios.post("/api/todos", {text: newTodo})
+     setTodos([...todos, response.data])
+     setNewTodo('');
+   }catch(error){
+      console.log('Error adding todo:', error);
+   }
+ } 
+  return (
+    <>
+      <div className='min-h-screen 
+        bg-gradient-to-br from gray-50to-gray-100
+          flex items-center justify-center p-4'>
+            <div className='bg-white rounded-2xl
+               shadow-xl w-full max-w-lg p-8'>
+                <div>
+                  <h1 className='text-4xl font-bold
+                    text-gray-800 mb-2'>
+                    Task Manager
+                    </h1>
+                </div>
+                <form className='flex items-center
+                   gap-2 shadow-sm border border-gray-200
+                    p-2 rounded-lg'>
+                  <input 
+                  className='flex-1 outline-none px-3 py-2
+                    text-gray-700 placeholder-gray-400'
+                  type="text" 
+                  value={newTodo}
+                  onChange={(e) => setNewTodo(e.target.value)}
+                  placeholder='what needs to be done?'
+                  required
+                  />
+                  <button className='bg-blue-500
+                     hover:bg-blue-600 text-white px-4 py-2
+                      rounded-md font-medium cursor-pointer'>
+                      Add Task
+                    </button>
+                </form>
+            </div>
+      </div>
+    </>
+  )
+}
+
+export default App
