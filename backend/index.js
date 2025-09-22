5939c902568822351d422e276f08a810e995c85f
Harshit <hktiwari980@gmail.com>
2025-09-22 21:35:06 +0530

    backend completed

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,19 @@
 import express from 'express';
 import dotenv from "dotenv";
+import todoRoutes from "./routes/todo.route.js";
 import { connectDB } from './config/db.js';
 
 dotenv.config();
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req,res) =>{
     res.send("Server is ready");
 });
+
+app.use("/api/todos", todoRoutes)
 app.listen(5000, () =>{
     connectDB();
     console.log("Server started at http://localhost:5000");
